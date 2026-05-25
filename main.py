f6c318390c5175705c692750e8748ac0bebbb4b3
manuelgalindo267 <iemlgalindo@gmail.com>
2026-05-24 22:52:11 -0500

    Implementar algoritmo BFS para encontrar rutas entre estaciones
    Utilizando deque para aplicar FIFO (First in, first out) se aplico un metodo de busqueda BFS (Breadth first search), tambien se agregaron logs que indican una lista de estaciones y le piden al user que ingrese la ruta de inicio y la ruta final

--- main.py
+++ main.py
@@ -41,5 +41,78 @@ conocimiento = {
 def conectado(estacion1, estacion2):
 
     return estacion2 in conocimiento[estacion1]
+    "Agregar función de reglas lógicas para conexiones"
 
- "Agregar función de reglas lógicas para conexiones"
+# ==========================
+# BUSQUEDA BFS
+# ==========================
+
+def encontrar_ruta(origen,destino):
+
+    cola=deque()
+
+    cola.append([origen])
+
+    visitados=[]
+
+
+    while cola:
+
+        ruta=cola.popleft()
+
+        estacion_actual=ruta[-1]
+
+
+        if estacion_actual==destino:
+
+            return ruta
+
+
+        if estacion_actual not in visitados:
+
+            visitados.append(estacion_actual)
+
+
+            for vecino in conocimiento[estacion_actual]:
+
+                if conectado(estacion_actual,vecino):
+            
+                    nueva_ruta=list(ruta)
+            
+                    nueva_ruta.append(vecino)
+            
+                    cola.append(nueva_ruta)
+
+    return None
+
+# ==========================
+# INTERFAZ
+# ==========================
+
+print("\n SISTEMA INTELIGENTE DE RUTAS\n")
+
+print("Estaciones disponibles:\n")
+
+for i in conocimiento:
+
+    print("-",i)
+
+origen=input("\nIngrese origen: ")
+
+destino=input("Ingrese destino: ")
+
+ruta=encontrar_ruta(origen,destino)
+
+if ruta:
+
+    print("\nRuta encontrada:\n")
+
+    for estacion in ruta:
+
+        print(estacion)
+
+    print("\nTotal estaciones:",len(ruta))
+
+else:
+
+    print("\nNo existe ruta")
